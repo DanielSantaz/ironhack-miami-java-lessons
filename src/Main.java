969917f165f8a410606a2686bc53722a551101a1
DanielSantaz <danielsantaz@gmail.com>
2024-06-11 09:24:59 -0400

    Java lessons project created

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+
+
+    public static void main(String[] args) {
+     //
+
+
+//        WRAPPERS:
+        Integer myNum = 100;
+
+        System.out.println(myNum);
+    }
+
+    public static void myMethod(){
+        System.out.println("This");
+        System.out.println("Is");
+        System.out.println("Inside");
+        System.out.println("myMethod!");
+    }
+}
