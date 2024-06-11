e8cf6539cfa7e3bec6a362756979415fb29ab75f
DanielSantaz <danielsantaz@gmail.com>
2024-06-11 10:29:28 -0400

    arrays tips and tricks

--- src/com/ironhack/w1/tuesday/Main.java
+++ src/com/ironhack/w1/tuesday/Main.java
@@ -1,7 +1,51 @@
 package com.ironhack.w1.tuesday;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Week 1 day 2 lessons");
+//        ARRRAYS
+
+        String[] daysOfTheWeek = new String[7];
+        for (int i = 0; i < daysOfTheWeek.length; i ++){
+            String day = "";
+            switch (i){
+                case 0:
+                    day += "monday";
+                    break;
+                case 1:
+                    day += "tuesday";
+                    break;
+                case 2:
+                    day += "wendesday";
+                    break;
+                case 3:
+                    day += "thursday";
+                    break;
+                case 4:
+                    day += "friday";
+                    break;
+                case 5:
+                    day += "saturday";
+                    break;
+                case 6:
+                    day += "sunday";
+                    break;
+            }
+
+
+            daysOfTheWeek[i] = day;
+            day = "";
+        }
+        System.out.println(daysOfTheWeek);
+        String arrayinfo = Arrays.toString(daysOfTheWeek);
+        arrayinfo = arrayinfo.substring(1,arrayinfo.length()-1);
+        arrayinfo = arrayinfo.replace(", ", "\n");
+        System.out.println(arrayinfo);
     }
+
+
+
+
 }
