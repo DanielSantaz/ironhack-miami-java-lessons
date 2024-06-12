8a378195251aee2a2165231c3dbb12f96d69afb9
DanielSantaz <danielsantaz@gmail.com>
2024-06-12 11:19:24 -0400

    wednesday

--- src/com/ironhack/w1/tuesday/Main.java
+++ src/com/ironhack/w1/tuesday/Main.java
@@ -1,6 +1,8 @@
 package com.ironhack.w1.tuesday;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -43,7 +45,42 @@ public class Main {
         arrayinfo = arrayinfo.substring(1,arrayinfo.length()-1);
         arrayinfo = arrayinfo.replace(", ", "\n");
         System.out.println(arrayinfo);
+
+
+        for (int i = 0; i < 7; i ++){
+            System.out.println(i);
+
+
     }
+        for (int i = 0; i < 101; i ++){
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+//        ARRAY LIST:
+        //will work better than arrays
+
+        List<String> countries = new ArrayList<String>();
+        countries.add("USA");
+        countries.add("Spain");
+        countries.add("France");
+        System.out.println(countries);
+        System.out.println(countries.get(0));
+
+        countries.set(2, "Italy"); //changing a value in a list.
+
+        Double d = 10d;
+        Double e = 9d;
+        Math.max(d,e);
+        System.out.println(Math.ceil(3.01));
+
+
+
+
+    }
+
+
 
 
 
---
+++ src/com/ironhack/w1/wednesday/Main.java
@@ -0,0 +1,79 @@
+package com.ironhack.w1.wednesday;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+// SCANNER MOMENT
+public class Main {
+    public static void main(String[] args) throws IOException {
+//
+//
+//
+//
+//        // SCANNER CLASS
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String name;
+//        do {
+//            System.out.println("Please write your name");
+//
+//            name = scanner.nextLine();
+//        } while (name.isEmpty());
+//
+//        name = name.substring(0,1).toUpperCase() + name.substring(1);
+//
+//        System.out.println("your name is " + name);
+//
+//
+//
+//
+//
+//        do {
+//            scanner = new Scanner(System.in);
+//            System.out.println("Please write your age!");
+//
+//        } while (!scanner.hasNextInt());
+//        int age = scanner.nextInt();
+//        System.out.println("your age is " + age);
+//
+//
+//
+//
+//        scanner.close();
+
+//        FILE HANDLING:
+
+
+        String filePath = "src/com/ironhack/w1/wednesday/file.txt";
+
+        File file = new File(filePath);
+        if (file.createNewFile()){
+            System.out.println("FILE HAS BEEN CREATED");
+        } else {
+            System.out.println("FILE ALREADY EXISTS");
+        }
+
+        FileWriter fileWriter = new FileWriter(file, true);
+        fileWriter.write("This is a line\n");
+        fileWriter.close();
+
+        Scanner fileScanner = new Scanner(file);
+
+        String str = "";
+
+
+
+        while(fileScanner.hasNextLine()){
+
+            str += fileScanner.nextLine() + "\n";
+        }
+
+        System.out.println(str);
+        fileScanner.close();
+
+    }
+
+}
