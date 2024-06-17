29b4d95907e296e75bc16a66c059ac4ed88de25b
DanielSantaz <danielsantaz@gmail.com>
2024-06-17 09:06:12 -0400

    starting the 2nd week

--- src/com/ironhack/w1/thursday/Main.java
+++ src/com/ironhack/w1/thursday/Main.java
@@ -1,8 +1,17 @@
 package com.ironhack.w1.thursday;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
+//BIG DECIMAL:
 
+        System.out.println(1.2-1.0);
+
+        BigDecimal bd1 = new BigDecimal("1.2");
+        BigDecimal bd2 = new BigDecimal("1.0");
+
+        System.out.println(bd1.subtract(bd2));
 
 
 
